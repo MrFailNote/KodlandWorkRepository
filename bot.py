3860d7f179d750367b71b03e76b0d1cf8dbaf78d
MrFailNote <smalysev750@gmail.com>
2024-08-05 20:36:57 +0700

    first commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+import telebot # библиотека telebot
+from config import token # импорт токена
+
+bot = telebot.TeleBot(token) 
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я бот для управления чатом.")
+
+@bot.message_handler(commands=['ban'])
+def ban_user(message):
+    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение
+        if len(message.text) > 4:
+            reason = message.text[4:] 
+        else: reason = "Бан"
+        
+        chat_id = message.chat.id # сохранение id чата
+         # сохранение id и статуса пользователя, отправившего сообщение
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+         # проверка пользователя
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен. По причине: {reason}")
+            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+        
+#@bot.message_handler(commands=['add'])
+#def add_user(message):
+    
+
+bot.infinity_polling(none_stop=True)
